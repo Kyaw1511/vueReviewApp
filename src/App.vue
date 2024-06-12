7a551e65f2525f2ed104e1133f93b6e3cc025d2c
Kyaw1511 <kyawminthein151193@gmail.com>
2024-06-12 23:16:19 +0630

    header.vue 1.0

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  
-
-  <RouterView />
+  <div>
+    <Header />
+    <RouterView />
+  </div>
 </template>
+
 <script setup>
   import { RouterView } from 'vue-router';
+  import Header from './components/Header.vue';
 </script>
-
